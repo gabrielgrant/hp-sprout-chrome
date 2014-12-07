5b47c3700ca80f7ad3c1d2d5faa6e35d7343e343
Gabriel Grant <g@briel.ca>
2014-12-06 19:53:04 -0700

    set up window to cover both screens

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
-      'width': 400,
-      'height': 500
-    }
+      'top': 0,
+	  'left': 0,
+	  'width': 1920,
+      'height': 1080 + 768  // cover both screens
+    },
+	'frame': {
+	  'type': 'none'
+	}
   });
 });
